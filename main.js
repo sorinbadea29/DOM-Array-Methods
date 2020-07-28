aaf20e98f8b625d251d99075cb369e1f43fb1de9
Sorin Badea <62335902+sorinbadea29@users.noreply.github.com>
2020-07-28 22:12:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,87 +11,95 @@ const calculateAllWealthBtn = document.getElementById("calculate-wealth");
 
 let persons = [];
 
-const moneyFormat = (number) => `$${number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}`;
+const formatMoney = 
+  number => `$${number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}`
 
-async function addRandomPerson() {
-  const res = await fetch("https://randomuser.me/api");
+async function addRandomPerson(){
+  const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
   const person = data.results[0];
   const newPerson = {
     gender: `${person.gender}`,
     name: `${person.name.first} ${person.name.last}`,
-    wealth: Math.floor(Math.random() * 1000000),
-  };
-  addNewPersonToPersons(newPerson);
-}
-
-function addNewPersonToPersons(obj) {
-  persons.push(obj);
+    wealth: Math.floor(Math.random() * 1000000)
+  }
+  pushInArray(newPerson);
   updateDOM(persons);
 }
 
-function doubleMoney() {
-  persons = persons.map((person) => {
-    return { ...person, wealth: person.wealth * 2 };
-  });
+function pushInArray(personObj){
+  persons.push(personObj);
+}
+
+function updateDOM(persons){
+  main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`;
+  persons.forEach(person => {
+    const personDiv = document.createElement('div');  
+    personDiv.classList.add('person');
+    const personNameDiv = document.createElement('div');  
+    personNameDiv.classList.add('personName');
+    const personWealthDiv = document.createElement('div');  
+    personWealthDiv.classList.add('personWealth');
+    personNameDiv.innerHTML = `<strong>${person.name}</strong>`;
+    personWealthDiv.innerHTML = `<p>${formatMoney(person.wealth)}</p>`;
+    personDiv.appendChild(personNameDiv);
+    personDiv.appendChild(personWealthDiv);
+    main.appendChild(personDiv);
+  })
+};
+
+function doubleMoney(){
+  persons = persons.map(person => {
+    return {...person, wealth: person.wealth* 2}
+  })
   updateDOM(persons);
 }
 
-function showOnlyMillionaires() {
-  persons = persons.filter((person) => person.wealth >= 1000000);
+function showOnlyMillionaires(){
+  const millionaires = person => person.wealth >= 1000000;
+  persons = persons.filter(millionaires);
   updateDOM(persons);
 }
 
-function showOnlyNonMillionaires() {
-  persons = persons.filter((person) => person.wealth <= 1000000);
+function showOnlyNonMillionaires(){
+  const nonMillionaires = person => person.wealth <= 1000000;
+  persons = persons.filter(nonMillionaires);
   updateDOM(persons);
 }
 
-function sortByRichest() {
-  const descendent = (a, b) => b.wealth - a.wealth;
-  persons.sort(descendent);
+function sortByRichest(){
+  const desc = (a, b) => b.wealth - a.wealth;
+  persons.sort(desc);
   updateDOM(persons);
 }
 
-function sortByPoorest() {
-  const ascendent = (a, b) => a.wealth - b.wealth;
-  persons.sort(ascendent);
+function sortByPoorest(){
+  const asc = (a, b) => a.wealth - b.wealth;
+  persons.sort(asc);
   updateDOM(persons);
 }
 
-function showOnlyFemales() {
-  persons = persons.filter((person) => person.gender === "female");
+function showOnlyFemales(){
+  const females = person => person.gender === 'female';
+  persons = persons.filter(females);
   updateDOM(persons);
 }
 
-function showOnlyMales() {
-  const males = (person) => person.gender === "male";
+function showOnlyMales(){
+  const males = person => person.gender === 'male';
   persons = persons.filter(males);
   updateDOM(persons);
 }
 
-function calculateAllWealth() {
-  totalWealth = persons.reduce((acc, person) => (acc += person.wealth), 0);
-  const totalWealthDiv = document.createElement("div");
-  totalWealthDiv.innerHTML = `<h3>Total Wealth:<strong>${moneyFormat(totalWealth)}</strong></h3>`;
-  main.appendChild(totalWealthDiv);
-}
-
-function updateDOM(persons) {
-  main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`;
-  persons.forEach((person) => {
-    const personDiv = document.createElement("div");
-    personDiv.classList.add("person");
-    const personNameDiv = document.createElement("div");
-    personNameDiv.classList.add("personName");
-    personNameDiv.innerHTML = `${person.name}`;
-    const personWealthDiv = document.createElement("div");
-    personWealthDiv.classList.add("personWealth");
-    personWealthDiv.innerHTML = `${moneyFormat(person.wealth)}`;
-    personDiv.appendChild(personNameDiv);
-    personDiv.appendChild(personWealthDiv);
-    main.appendChild(personDiv);
-  });
+function calculateEntireWealth(){
+  const total = ((acc, item) => (acc += item));
+  const totalWealth = persons
+    .map(person => person.wealth)
+    .reduce((total), 0);
+  const totalEl = document.createElement('div');
+  totalEl.innerHTML = `
+    <h3><strong>Total Wealth: </strong>${formatMoney(totalWealth)}</h3>`;
+  main.appendChild(totalEl);
 }
 
 // Buttons Events
@@ -99,8 +107,8 @@ addPersonBtn.addEventListener("click", addRandomPerson);
 doubleMoneyBtn.addEventListener("click", doubleMoney);
 showOnlyMillionairesBtn.addEventListener("click", showOnlyMillionaires);
 showOnlyNonMillionairesBtn.addEventListener("click", showOnlyNonMillionaires);
-sortByPoorestBtn.addEventListener("click", sortByPoorest);
 sortByRichestBtn.addEventListener("click", sortByRichest);
+sortByPoorestBtn.addEventListener("click", sortByPoorest);
 showOnlyFemalesBtn.addEventListener("click", showOnlyFemales);
 showOnlyMalesBtn.addEventListener("click", showOnlyMales);
-calculateAllWealthBtn.addEventListener("click", calculateAllWealth);
+calculateAllWealthBtn.addEventListener("click", calculateEntireWealth);
